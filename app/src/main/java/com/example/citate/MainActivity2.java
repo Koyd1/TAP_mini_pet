e558031b86e45e52bca6aadbdb48e63766cd8503
Really_true_dude <andrekor2004@gmail.com>
2025-03-11 22:08:51 +0200

    Intent to quotes

--- app/src/main/java/com/example/citate/MainActivity2.java
+++ app/src/main/java/com/example/citate/MainActivity2.java
@@ -2,12 +2,14 @@ package com.example.citate;
 
 //import com.example.citate.databinding.MainBinding;
 
-import java.sql.Array;
 import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.widget.ListView;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+
+import android.view.View;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +22,7 @@ public class MainActivity2 extends AppCompatActivity {
     String[] authors = {"Фалькон", "Булэйт", "Автор 1", "Автор 2"};
     String[] authors_desc = {"Лучший в мире дед.", "Сэ трэиць домнул колонел", "Бла-бла-бла-бла-бла", "Бла-бла-бла-бла-бла"};
     int[] authors_images = {R.drawable.test, R.drawable.anime, R.drawable.test, R.drawable.test};
+    String[] falco_quotes = {"Женскому уму это не понять.", "Политех не учит программированию, политех учит жизни."};
     ArrayList<AuthorData> dataList = new ArrayList<AuthorData>();
 
 
@@ -43,6 +46,18 @@ public class MainActivity2 extends AppCompatActivity {
         ListView listView = findViewById(R.id.author_list_view);
         listView.setAdapter(authorsAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                intent.putExtra("authorName", authors[position]);
+                intent.putExtra("authorDesc", authors_desc[position]);
+                intent.putExtra("authorImage", authors_images[position]);
+                intent.putExtra("authorQuotes", falco_quotes);
+                startActivity(intent);
+            }
+
+        });
 
     }
 }
--- app/src/main/java/com/example/citate/MainActivity3.java
+++ app/src/main/java/com/example/citate/MainActivity3.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.content.Intent;
+import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -11,6 +17,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    int currQuote = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +35,59 @@ public class MainActivity3 extends AppCompatActivity {
         String[] arr = {"Волк", "Обезьяна", "Собака"};
 
         Spinner spinner = findViewById(R.id.action_bar_spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, arr);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, arr);
         adapter.setDropDownViewResource(R.layout.mylayout);
 
         spinner.setAdapter(adapter);
+
+
+        ImageView image = findViewById(R.id.imageView);
+        TextView quote = findViewById(R.id.textView);
+        TextView authorName = findViewById(R.id.authorNameView);
+        TextView quoteNum = findViewById(R.id.quoteNumView);
+
+        ImageView arrowRight = findViewById(R.id.arrow_right);
+        ImageView arrowLeft = findViewById(R.id.arrow_left);
+
+        Intent intent = this.getIntent();
+        if(intent != null) {
+            image.setImageResource(intent.getIntExtra("authorImage", R.drawable.arrow_left));
+            authorName.setText(intent.getStringExtra("authorName"));
+            String[] quotesArr = intent.getStringArrayExtra("authorQuotes");
+            if (quotesArr != null) {
+                quote.setText(quotesArr[0]);
+                quoteNum.setText("Цитата №" + 0);
+
+                // Next quote
+                arrowRight.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        currQuote++;
+                        if (currQuote == quotesArr.length) {
+                            currQuote = 0;
+                        }
+                        quote.setText(quotesArr[currQuote]);
+                        quoteNum.setText("Цитата №" + currQuote);
+                    }
+                });
+
+                // Previous quote
+                arrowLeft.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (currQuote > 0) {
+                            currQuote--;
+                            quote.setText(quotesArr[currQuote]);
+                            quoteNum.setText("Цитата №" + currQuote);
+                        }
+
+                    }
+                });
+            }
+            else
+                quote.setText("Цитаты не найдены.");
+
+        }
 
     }
 }
